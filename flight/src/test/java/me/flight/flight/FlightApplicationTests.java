9d0b0846fee9530a399b0aca304e46ca46a29387
filipe-fff <pintovicentinifilipe@gmail.com>
2024-11-17 06:58:31 -0300

    estrutura basica do projeto feito

---
+++ flight/src/test/java/me/flight/flight/FlightApplicationTests.java
@@ -0,0 +1,13 @@
+package me.flight.flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
